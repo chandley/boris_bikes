5072b8b7e8fcfed2e3e9bd1361156696c334a33d
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-28 16:33:55 +0000

    Added fixing to bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -11,4 +11,11 @@ describe Bike do
     expect(bike).to be_broken
   end
 
+  it 'should be able to be fixed' do
+    bike = Bike.new
+    bike.break!
+    bike.fix!
+    expect(bike).not_to be_broken
+  end
+
 end
