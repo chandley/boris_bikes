b98c4b76034d954fe0247d59142c9541aa7f4e75
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-28 20:43:39 +0000

    refactor docking station test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,24 +2,24 @@ require 'docking_station'
 
 describe DockingStation do 
 
-
+  let(:bike) {Bike.new}
+  let(:station) {DockingStation.new}
 
   it 'should accept a bike' do
-    bike = Bike.new
-    station = DockingStation.new
     expect(station.bike_count).to eq(0)
     station.dock(bike)
     expect(station.bike_count).to eq(1)  
   end
 
   it 'should release a bike' do
-    bike = Bike.new
-    station = DockingStation.new
     station.dock(bike)
     bike = station.release
     expect(station.bike_count).to eq(0)
-
   end
 
+  it 'should know when it is full' do
+
+
+  end
   
 end
