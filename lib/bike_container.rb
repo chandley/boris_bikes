0c881a7192c499c768c6bf7bf6810bad54215e57
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-30 18:58:47 +0000

    added move function to bike container

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -36,4 +36,9 @@ DEFAULT_CAPACITY = 20
     bikes.reject { |bike| bike.broken? }
   end
 
+  def move(bike,from_container,to_container)
+    from_container.release(bike)
+    to_container.dock(bike)  
+  end
+
 end
