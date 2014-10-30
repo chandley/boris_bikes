d967c75db04ba6852d71b1460dbeb83bfdb92c77
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-30 13:44:17 +0000

    doubles with changing properties - fix garage

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -3,16 +3,17 @@ require 'garage'
 describe Garage do 
 
   let(:garage) { Garage.new(:capacity => 123) }
-  let(:bike) { Bike.new }
+  let(:bike) {double :bike, :broken? => true, :is_a? => true }
   
   it "should allow setting default capacity on initialising" do
     expect(garage.capacity).to eq(123)
   end
   
   it 'should be able to fix bikes' do
-    bike.break!
     garage.dock(bike)
     expect(garage.available_bikes.size).to be(0)
+    allow(bike).to receive(:fix!)
+    allow(bike).to receive(:broken?).and_return(false) 
     garage.fix_bikes # maybe change to fix individual bikes
     expect(garage.available_bikes.size).to be(1)
   end
