12173e4b7bc284ef1ed51ab222863246ee55b965
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-31 10:47:57 +0000

    van tests all part refactored for doubling

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -7,11 +7,7 @@ describe Van do
   let(:broken_bike) {double :bike, is_a?: true, 
                                    broken?: true }
   let(:docking_station) {double :docking_station , is_a?: true}
-  let(:garage) {double :garage , available_bikes: [:bike], 
-                                 broken_bikes: [:broken_bike],
-                                 release: nil, 
-                                 dock: nil,
-                                 class?: Garage}
+  let(:garage) {double :garage }
 
   # let(:docking_station) {DockingStation.new}
   # let(:garage) {Garage.new}
@@ -30,22 +26,28 @@ describe Van do
   end
 
   it 'delivers all fixed bikes to docking station' do
+    allow(docking_station).to receive(:broken_bikes).and_return([])
     allow(docking_station).to receive(:dock)
     van.dock(bike)
     van.replenish(docking_station)
     expect(van.available_bikes).to eq([])
   end
 
-  # it 'loads all bikes from garage' do
-  #   garage.dock(bike)
-  #   van.replenish(garage)
-  #   expect(van.available_bikes).to eq([bike])
-  # end
+  it 'loads all bikes from garage' do
+    allow(garage).to receive(:is_a?).and_return(false,true) # bad
+    allow(garage).to receive(:available_bikes).and_return([bike])
+    allow(garage).to receive(:release)
+    van.replenish(garage)
+    expect(van.available_bikes).to eq([bike])
+  end
 
-  #  it 'delivers all broken bikes to garage' do
-  #   van.dock(broken_bike)
-  #   van.replenish(garage)
-  #   expect(van.broken_bikes).to eq([])
-  # end
+   it 'delivers all broken bikes to garage' do
+    allow(garage).to receive(:is_a?).and_return(false,true) # bad
+    van.dock(broken_bike)
+    allow(garage).to receive(:available_bikes).and_return([])
+    allow(garage).to receive(:dock)
+    van.replenish(garage)
+    expect(van.broken_bikes).to eq([])
+  end
 
 end
